1bf4a1cb3fc89c526d8ff642f31f7c624495ae1d
Gabe <gabriel.gelgor@ontariotechu.net>
2021-07-12 15:17:07 -0400

    Working on sorter example for better grasp of reusability via interfaces

--- 5_sort/src/index.ts
+++ 5_sort/src/index.ts
@@ -1,7 +1,15 @@
-console.log(123);
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const logSomething = () => {
-    console.log('abc');
-};
+const numColl = new NumbersCollection([-1000,3,-5,0])
+const charCol = new CharactersCollection("Gabriel");
 
-logSomething();
+const sorter = new Sorter(numColl);
+sorter.sort();
+console.log(`Numbers: ${numColl.data}`);
+
+const sorterC = new Sorter(charCol);
+sorterC.sort();
+console.log(`Letters: ${charCol.data}`);
+
